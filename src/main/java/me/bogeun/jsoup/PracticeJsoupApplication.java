7a76964f0dffbad0d4e459e0bbaff0c7ca630571
idiot2222 <pky2892@gmail.com>
2021-07-17 16:18:15 +0900

    init

---
+++ src/main/java/me/bogeun/jsoup/PracticeJsoupApplication.java
@@ -0,0 +1,13 @@
+package me.bogeun.jsoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJsoupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeJsoupApplication.class, args);
+    }
+
+}
